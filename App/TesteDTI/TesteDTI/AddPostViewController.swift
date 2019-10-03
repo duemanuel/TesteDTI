ce4eb902504c1fc31ebf52f7574c078e50938cf8
Eduardo Oliveira <eduardo.oliveira3d@gmail.com>
2019-10-03 00:40:11 -0300

    Finalizando

--- App/TesteDTI/TesteDTI/AddPostViewController.swift
+++ App/TesteDTI/TesteDTI/AddPostViewController.swift
@@ -21,14 +21,28 @@ class AddPostViewController: UIViewController {
 	
 	@IBAction func savePost(_ sender: UIButton) {
 		
-		let date = Date()
-		let formatter = DateFormatter()
-		formatter.dateFormat = "dd/MM/yyyy"
-		let resultDate = formatter.string(from: date)
-		
-		let newPost = BlogInfo(data: resultDate, title: tfTitle.text ?? "", description: tfDescription?.text ?? "")
-		BlogApi.saveNewPost(post: newPost)
-		self.dismiss(animated: true, completion: nil)
+		if let text = tfDescription.text, !text.isEmpty {
+			if let text = tfTitle.text, !text.isEmpty {
+				let date = Date()
+				let formatter = DateFormatter()
+				formatter.dateFormat = "dd/MM/yyyy"
+				let resultDate = formatter.string(from: date)
+				
+				let newPost = BlogInfo(data: resultDate, title: tfTitle.text ?? "", description: tfDescription?.text ?? "")
+				BlogApi.saveNewPost(post: newPost)
+				self.dismiss(animated: true, completion: nil)
+			}
+			else{
+				let alert = UIAlertController(title: "Atenção", message: "O campo do título deve ser preenchido.", preferredStyle: UIAlertController.Style.alert)
+				alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
+				self.present(alert, animated: true, completion: nil)
+			}
+		}
+		else{
+			let alert = UIAlertController(title: "Atenção", message: "O campo da descrição deve ser preenchida.", preferredStyle: UIAlertController.Style.alert)
+			alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
+			self.present(alert, animated: true, completion: nil)
+		}
 	}
 	
 	@IBAction func btnVoltar(_ sender: UIButton) {
